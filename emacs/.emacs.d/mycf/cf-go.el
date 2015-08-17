e65551625f689c2fbae027fd8491ce5a397c6677
Xiao Jiang <showbufire@gmail.com>
2015-08-16 21:43:02 -0700

    [emacs] go imports and go oracle

--- emacs/.emacs.d/mycf/cf-go.el
+++ emacs/.emacs.d/mycf/cf-go.el
@@ -1,6 +1,15 @@
 ;;; source http://tleyden.github.io/blog/2014/05/22/configure-emacs-as-a-go-editor-from-scratch/
 
+;;; PREREQUISITES:
+;;; install go
+;;; setup $GOPATH $GOROOT
+;;; go get golang.org/x/tools/cmd/goimports
+;;; go get golang.org/x/tools/cmd/oracle && sudo mv $GOPATH/bin/oracle $GOROOT/bin/
+
+
 (defun my-go-mode-hook ()
+  ; Use goimports instead of go-fmt
+  (setq gofmt-command "goimports")
   ; Call Gofmt before saving
   (add-hook 'before-save-hook 'gofmt-before-save)
   ; Customize compile command to run go build
@@ -10,3 +19,4 @@
   ; Godef jump key binding
   (local-set-key (kbd "M-.") 'godef-jump))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
+(load-file "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el")
