7d384c000af831c68983eb27a6b8bafe311e1711
Xiao Jiang <xiaoj@fb.com>
2017-11-08 12:28:36 -0800

    [powershell] add pmarks.ps1

---
+++ powershell/.psrc/pmarks.ps1
@@ -0,0 +1,20 @@
+function marks {
+    python $pmarks ls
+}
+
+function mark($name) {
+    python $pmarks mark $name
+}
+
+function unmark($name) {
+    python $pmarks unmark $name
+}
+
+function jump($name) {
+    $dest = python $pmarks get $name
+    if ($?) {
+        Set-Location -Path $dest -PassThru
+    }
+}
+
+Set-Alias -Name jp -Value jump
